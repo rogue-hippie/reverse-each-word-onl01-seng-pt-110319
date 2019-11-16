d315b227af18c9277c873266135688fa5afdb9a6
Whitney Hodgkins <hodgkins.whitney@gmail.com>
2019-11-16 03:13:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,6 @@
-def reverse_each_word(sentence1)
+def reverse_each_word(sentence1, sentence2)
   sentence1 = "Hello there, and how are you?".split(/ /)
   sentence1.collect{|a| a.reverse}.join(" ")
+  
+  sentence2 = ""
 end
